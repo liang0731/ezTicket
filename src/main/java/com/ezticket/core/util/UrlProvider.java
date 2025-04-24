8bf5bc19d1e013a0f9d7363dd6d47d67615d1f49
Jie <liangjie0731@gmail.com>
2025-04-24 23:41:22 +0800

    更新配置以使用動態生成的返回URL，移除ngrok相關設置；重構URL生成邏輯以提高可讀性和可維護性

--- src/main/java/com/ezticket/core/util/UrlProvider.java
+++ src/main/java/com/ezticket/core/util/UrlProvider.java
@@ -6,54 +6,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Component
 public class UrlProvider {
 
-    @Value("${spring.profiles.active}")
-    private String active;
-
-    @Value("${ngrok.url}")
-    private String ngrokUrl;
+    @Value("${ecpay.return.url}")
+    @Getter
+    private String returnURL;
 
     @Value("${server.port}")
     private String serverPort;
 
     @Getter
-    private String returnURL;
-    @Getter
     private String localURL;
 
     @PostConstruct
     public void init() {
-        this.returnURL = getBaseUrl();
-        this.localURL = getLocalHostUrl();
+        this.localURL = buildLocalHostUrl();
     }
 
-    private String getBaseUrl() {
-        if ("dev".equalsIgnoreCase(active)) {
-            // 本機測試環境，使用 Ngrok URL
-            return ngrokUrl;
-        } else {
-            // 正式環境，自動抓取部屬的主機名稱
-            try {
-                InetAddress localHost = InetAddress.getLocalHost();
-                String ipAddress = localHost.getHostAddress();
-                return "https://" + ipAddress + ":" + serverPort;
-            } catch (UnknownHostException e) {
-                throw new RuntimeException("無法取得主機名稱", e);
-            }
-        }
-    }
-
-    private String getLocalHostUrl() {
+    private String buildLocalHostUrl() {
         try {
             InetAddress localHost = InetAddress.getLocalHost();
-            String ipAddress = localHost.getHostAddress();
-            return "http://" + ipAddress + ":" + serverPort;
-        } catch (UnknownHostException e) {
-            throw new RuntimeException("無法取得主機名稱", e);
+            return "http://" + localHost.getHostAddress() + ":" + serverPort;
+        } catch (Exception e) {
+            throw new RuntimeException("無法取得本機 IP", e);
         }
     }
 }
--- src/main/java/ecpay/payment/integration/ecpayOperator/PaymentVerifyBase.java
+++ src/main/java/ecpay/payment/integration/ecpayOperator/PaymentVerifyBase.java
@@ -1,79 +1,80 @@
 package ecpay.payment.integration.ecpayOperator;
 
-import java.io.File;
-import java.net.URL;
+import ecpay.payment.integration.errorMsg.ErrorMessage;
+import ecpay.payment.integration.exception.EcpayException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import ecpay.payment.integration.errorMsg.ErrorMessage;
-import ecpay.payment.integration.exception.EcpayException;
+public class PaymentVerifyBase {
+    protected String confPath = "ecpay/payment/integration/config/EcpayPayment.xml";
+    protected Document doc;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-public class PaymentVerifyBase{
-
-	protected String confPath = "src/main/java/ecpay/payment/integration/config/EcpayPayment.xml";
-	protected Document doc;
-	public PaymentVerifyBase(){
-		try {
-			File configFile = new File(confPath);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			doc = dBuilder.parse(configFile);
-			doc.getDocumentElement().normalize();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public PaymentVerifyBase() {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(confPath)) {
+            if (is == null) {
+                System.out.println("找不到設定檔：" + confPath);
+                return;
+            }
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(is);
+            doc.getDocumentElement().normalize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
-	protected void requireCheck(String FieldName, String objValue, String require){
-		if(require.equals("1") && objValue.isEmpty())
-			throw new EcpayException(FieldName+"為必填");
-	}
-	
-	protected void valueCheck(String type, String objValue, Element ele){
-		if(objValue.isEmpty()){
-			return;
-		}
-		if(type.equals("String")){
-			if(ele.getElementsByTagName("pattern") != null){
-				Pattern r = Pattern.compile(ele.getElementsByTagName("pattern").item(0).getTextContent().toString());
-				Matcher m = r.matcher(objValue);
-				if(!m.find())
-					throw new EcpayException(ele.getAttribute("name")+ErrorMessage.COLUMN_RULE_ERROR);
-			}
-		} else if(type.equals("Opt")){
-			List<String> opt = new ArrayList<String>();
-			NodeList n = ele.getElementsByTagName("option");
-			for(int i=0; i < n.getLength(); i++){
-				opt.add(n.item(i).getTextContent().toString());
-			}
-			if(!opt.contains(objValue))
-				throw new EcpayException(ele.getAttribute("name")+ErrorMessage.COLUMN_RULE_ERROR);
-		} else if(type.equals("Int")){
-			String mode = ele.getElementsByTagName("mode").item(0).getTextContent();
-			String minimum = ele.getElementsByTagName("minimal").item(0).getTextContent();
-			String maximum = ele.getElementsByTagName("maximum").item(0).getTextContent();
-			if(objValue.isEmpty())
-				throw new EcpayException(ele.getAttribute("name")+ErrorMessage.CANNOT_BE_EMPTY);
-			int value = Integer.valueOf(objValue);
-			if(mode.equals("GE") && value < Integer.valueOf(minimum))
-				throw new EcpayException(ele.getAttribute("name")+"不能小於"+minimum);
-			else if(mode.equals("LE") && value > Integer.valueOf(maximum))
-				throw new EcpayException(ele.getAttribute("name")+"不能大於"+maximum);
-			else if(mode.equals("BETWEEN") && value < Integer.valueOf(minimum) && value > Integer.valueOf(maximum))
-				throw new EcpayException(ele.getAttribute("name")+"必須介於"+minimum+"和"+maximum+"之間");
-			else if(mode.equals("EXCLUDE") && value >= Integer.valueOf(minimum) && value <= Integer.valueOf(maximum))
-				throw new EcpayException(ele.getAttribute("name")+"必須小於"+minimum+"或大於"+maximum);
-		} else if(type.equals("DepOpt")){
-			// TODO
-		} 
-	}
+    protected void requireCheck(String FieldName, String objValue, String require) {
+        if (require.equals("1") && objValue.isEmpty())
+            throw new EcpayException(FieldName + "為必填");
+    }
+
+    protected void valueCheck(String type, String objValue, Element ele) {
+        if (objValue.isEmpty()) {
+            return;
+        }
+        if (type.equals("String")) {
+            if (ele.getElementsByTagName("pattern") != null) {
+                Pattern r = Pattern.compile(ele.getElementsByTagName("pattern").item(0).getTextContent().toString());
+                Matcher m = r.matcher(objValue);
+                if (!m.find())
+                    throw new EcpayException(ele.getAttribute("name") + ErrorMessage.COLUMN_RULE_ERROR);
+            }
+        } else if (type.equals("Opt")) {
+            List<String> opt = new ArrayList<String>();
+            NodeList n = ele.getElementsByTagName("option");
+            for (int i = 0; i < n.getLength(); i++) {
+                opt.add(n.item(i).getTextContent().toString());
+            }
+            if (!opt.contains(objValue))
+                throw new EcpayException(ele.getAttribute("name") + ErrorMessage.COLUMN_RULE_ERROR);
+        } else if (type.equals("Int")) {
+            String mode = ele.getElementsByTagName("mode").item(0).getTextContent();
+            String minimum = ele.getElementsByTagName("minimal").item(0).getTextContent();
+            String maximum = ele.getElementsByTagName("maximum").item(0).getTextContent();
+            if (objValue.isEmpty())
+                throw new EcpayException(ele.getAttribute("name") + ErrorMessage.CANNOT_BE_EMPTY);
+            int value = Integer.valueOf(objValue);
+            if (mode.equals("GE") && value < Integer.valueOf(minimum))
+                throw new EcpayException(ele.getAttribute("name") + "不能小於" + minimum);
+            else if (mode.equals("LE") && value > Integer.valueOf(maximum))
+                throw new EcpayException(ele.getAttribute("name") + "不能大於" + maximum);
+            else if (mode.equals("BETWEEN") && value < Integer.valueOf(minimum) && value > Integer.valueOf(maximum))
+                throw new EcpayException(ele.getAttribute("name") + "必須介於" + minimum + "和" + maximum + "之間");
+            else if (mode.equals("EXCLUDE") && value >= Integer.valueOf(minimum) && value <= Integer.valueOf(maximum))
+                throw new EcpayException(ele.getAttribute("name") + "必須小於" + minimum + "或大於" + maximum);
+        } else if (type.equals("DepOpt")) {
+            // TODO
+        }
+    }
 }
